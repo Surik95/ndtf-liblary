c3d1c36fe64eca5c93667082692ea548efb68597
Surik95 <pokemon.surik@gmail.com>
2023-09-18 19:10:55 +0300

    выполнил задание

--- src/abstract-class/BooksRepository.ts
+++ src/abstract-class/BooksRepository.ts
@@ -3,28 +3,17 @@ export default abstract class BooksRepository {
   arrBook: Book[];
 
   //создание книги
-  createBook(book: Book): void {
-    this.arrBook.push(book);
-  }
+  abstract createBook(book: Book): void;
 
   //получение книги по id
-  getBook(id: string): Book | undefined {
-    return this.arrBook.find((item) => item.id === id);
-  }
+  abstract getBook(id: string): Book | undefined;
 
   //получение всех книг
-  getBooks(): Book[] {
-    return this.arrBook;
-  }
+  abstract getBooks(): Book[];
 
   //обновление книги
   abstract updateBook(id: string): void;
 
   //удаление книги
-  deleteBook(id: string): void {
-    const indexBook: number = this.arrBook.findIndex((item) => item.id === id);
-    if (indexBook !== -1) {
-      this.arrBook.splice(indexBook, 1);
-    }
-  }
+  abstract deleteBook(id: string): void;
 }
